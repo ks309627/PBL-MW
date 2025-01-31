16cf52c2753153fb91ec243397e2352fcb4b320a
tz309649 <tz309649@student.polsl.pl>
2025-01-31 19:52:43 +0100

    Quick fix (updated ESPCom)

--- ESPCom.py
+++ ESPCom.py
@@ -4,13 +4,37 @@ from settings import Settings
 from LoggingHandler import Logger
 
 class ESPCom: #changed SerialCommunicator to ESPCom
-    def __init__(self, settings: Settings, baud_rate=9600, timeout=1):
+    def __init__(self, settings: Settings, baudrate=9600, timeout=1):
         self.logger = Logger()
         self.settings = settings
         self.port = self.settings.get("COMPathESP")
-        self.baud_rate = baud_rate
+        self.baudrate = baudrate
         self.timeout = timeout
         self.serial_connection = None
+        try:
+            self.ser = serial.Serial(port = self.port, baudrate = self.baudrate, timeout= self.timeout)
+        except serial.SerialException as e:
+            self.logger.log_warning(f"Failed to open COM port: {e}")
+        self.initialized = True
+    
+    def cmd_custom(self, command, silent=False):
+        full_command = f"{command}\r\n"
+        self.ser.write(full_command.encode('utf-8'))
+        self.logger.log_info(f"ESP - Wysłano komendę: {full_command.strip()}")
+        response = self.ser.readline().decode('utf-8', errors='ignore').strip()
+        if response:
+            print(f"ESP - Otrzymano wiadomość: {response}")
+            return response
+    
+    def connection_close(self):
+        self.logger.log_info("ESP Connection closed")
+        self.ser.close()
+
+    def update_com_path(self, new_port):
+        self.port = new_port
+        self.logger.log_info(f"[INFO]: Port COM zaktualizowany na {self.port}")
+    
+    #nw czy to będzie działać
     def connect(self):
         try:
             self.serial_connection = serial.Serial(self.port, self.baud_rate, timeout=1)
@@ -18,30 +42,3 @@ class ESPCom: #changed SerialCommunicator to ESPCom
         except Exception as e:
             return False, f"Error: {e}"
 
-    def update_com_path(self, new_port):
-        self.port = new_port
-        self.logger.log_info(f"[INFO]: Port COM zaktualizowany na {self.port}")
-
-
-    def send_command(self, command):
-        if self.serial_connection and self.serial_connection.is_open:
-            try:
-                self.serial_connection.write(command.encode())
-                response = self.serial_connection.read_all().decode().strip()
-                return f"Sent: {command}\nResponse: {response}"
-            except Exception as e:
-                return f"Error: {e}"
-        else:
-            return "Serial connection is not open"
-
-    def send_left_command(self):
-        return self.send_command("left")
-
-    def send_right_command(self):
-        return self.send_command("right")
-
-    def close(self):
-        if self.serial_connection and self.serial_connection.is_open:
-            self.serial_connection.close()
-            return "Connection closed"
-        return "Connection already closed"
