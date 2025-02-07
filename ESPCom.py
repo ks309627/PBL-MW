2e4f80f1e1d8e91efd1fc644a7a9ae300fcca40e
ks309627 <ks309627@student.polsl.pl>
2025-02-07 13:55:37 +0100

    - ESP update

--- ESPCom.py
+++ ESPCom.py
@@ -6,13 +6,13 @@ import time
 class ESPCom:
 
     _instance = None
-
-    def __new__(cls, settings: Settings, baudrate=9600, timeout=1):
+    
+    def __new__(cls, settings: Settings, baudrate=115200, timeout=1):
         if cls._instance is None:
             cls._instance = super(ESPCom, cls).__new__(cls)
         return cls._instance
     
-    def __init__(self, settings: Settings, baudrate=9600, timeout=1):
+    def __init__(self, settings: Settings, baudrate=115200, timeout=1):
 
         if not hasattr(self, 'initialized'):
             self.logger = Logger()
@@ -30,12 +30,9 @@ class ESPCom:
                 )
                 self.logger.log_info(f"Połączono z ESP na porcie {self.port}")
             except serial.SerialException as e:
-                self.logger.log_warning(f"Nie udało się otworzyć portu COM: {e}")
+                self.logger.log_warning(f"Nie udaÅo siÄ otworzyÄ portu COM: {e}")
 
             self.initialized = True  # Flaga inicjalizacji
-
-
-
 
     def connect(self):
         try:
@@ -47,36 +44,40 @@ class ESPCom:
             self.logger.log_info(f"Połączono z {self.port} przy {self.baudrate} bps")
             self.initialized = True
 
-            self.serial_connection.write(b"test\n")
+            self.serial_connection.write(b"1\r\n")
+            time.sleep(1)
             response = self.serial_connection.readline().decode('utf-8', errors='ignore').strip()
             if response:
-                self.logger.log_info(f"ESP odpowiedział: {response}")
+                self.logger.log_info(f"ESP odpowiedziaÅ: {response}")
                 return True, f"Connected to {self.port} at {self.baudrate} bps"
             else:
                 self.logger.log_warning("Brak odpowiedzi od ESP")
                 return False, "ESP nie odpowiada"
         except serial.SerialException as e:
-            self.logger.log_warning(f"Nie udało się otworzyć portu COM: {e}")
+            self.logger.log_warning(f"Nie udaÅo siÄ otworzyÄ portu COM: {e}")
             return False, str(e)
         
     def cmd_custom(self, command, silent=False):
 
         if not self.initialized or not self.serial_connection or not self.serial_connection.is_open:
-            self.logger.log_error("ESP nie jest połączone")
+            self.logger.log_error("ESP nie jest poÅÄczone")
             return None
 
         full_command = f"{command}\n"
         self.serial_connection.write(full_command.encode('utf-8'))
-        self.logger.log_info(f"Wysłano: {full_command.strip()}")
+        self.logger.log_info(f"WysÅano: {full_command.strip()}")
 
         response = self.serial_connection.readline().decode('utf-8', errors='ignore').strip()
         if response:
             print(f"Odpowiedź ESP: {response}")
-            return response
-        
+            self.cur_esp_pos = response
+            return response  
         return None
 
     def connection_close(self):
         if self.serial_connection and self.serial_connection.is_open:
             self.serial_connection.close()
             self.logger.log_info("Połączenie z ESP zamknięte")
+
+    def getLastResponse(self):
+        return self.cur_esp_pos
