d6dfe3c9d350d8ad8f5f9d9067e1b940eb397e5a
ks309627 <ks309627@student.polsl.pl>
2025-02-11 12:09:23 +0100

    First try correc icon loading.

--- GraphList.py
+++ GraphList.py
@@ -53,27 +53,30 @@ class GraphList:
         if self.graphs_number == 0:
             self.reset_index_marker = True
         self.model.setRowCount(0)
-        folders = [folder for folder in os.listdir(self.path) if os.path.isdir(os.path.join(self.path, folder))]
-        for folder in reversed(folders):
-            folder_path = os.path.join(self.path, folder)
-            if os.path.isdir(folder_path) and any(file.endswith('.json') for file in os.listdir(folder_path)):
-                match = re.match(r'measurement_(\d{4}-\d{2}-\d{2})_(\d{2}-\d{2}-\d{2})', folder)
-                if match:
-                    date_str = match.group(1)
-                    time_str = match.group(2)
-                    date_time_str = f"{date_str.replace('-', '.')} {time_str.replace('-', ':')}"
-                    display_name = f"Pomiar [{date_time_str}]"
-                else:
-                    display_name = folder
 
-                icon_path = self.find_icon(folder_path)
-                item = QStandardItem(display_name)
-                item.setData(folder, Qt.UserRole)
-                if icon_path:
-                    item.setIcon(QIcon(icon_path))
-                else:
-                    item.setIcon(self.default_icon)
-                self.model.appendRow(item)
+        valid_folders = [folder for folder in os.listdir(self.path) 
+                        if os.path.isdir(os.path.join(self.path, folder)) 
+                        and any(file.endswith('.json') for file in os.listdir(os.path.join(self.path, folder)))]
+
+        for folder in reversed(valid_folders):
+            folder_path = os.path.join(self.path, folder)
+            match = re.match(r'measurement_(\d{4}-\d{2}-\d{2})_(\d{2}-\d{2}-\d{2})', folder)
+            if match:
+                date_str = match.group(1)
+                time_str = match.group(2)
+                date_time_str = f"{date_str.replace('-', '.')} {time_str.replace('-', ':')}"
+                display_name = f"Pomiar [{date_time_str}]"
+            else:
+                display_name = folder
+
+            icon_path = self.find_icon(folder_path)
+            item = QStandardItem(display_name)
+            item.setData(folder, Qt.UserRole)
+            if icon_path:
+                item.setIcon(QIcon(icon_path))
+            else:
+                item.setIcon(self.default_icon)
+            self.model.appendRow(item)
         self.listView.setModel(self.model)
         self.listView.selectionModel().selectionChanged.connect(self.selection_changed)
         
